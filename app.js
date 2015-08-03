68a799b7e6b739850af030d08914c4fb837a15df
Alex <alexadeschamps@gmail.com>
2015-08-03 14:27:15 -0400

    added in stuff to lib

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-var express    = require('express'),
-    app        = express(),
-    morgan     = require('morgan'),
-    bodyParser = require('body-parser'),
-    fs         = require('fs'),
-    ejs        = require('ejs');
+var express        = require('express'),
+    app            = express(),
+    morgan         = require('morgan'),
+    bodyParser     = require('body-parser'),
+    fs             = require('fs'),
+    ejs            = require('ejs'),
+    expressLayouts = require('express-ejs-layouts');
+    paramsChecker  = require('./lib/middleware/params_checker');
 
 app.use(morgan('short'));
+console.log(__dirname + 'app/assets');
+app.use(express.static(__dirname + '/app/assets'));
+app.use(expressLayouts);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(paramsChecker);
+
+
+app.set('views', __dirname + '/app/views');
+app.set("view engine", "ejs");
+app.set('layout', 'layout/layout.ejs');
+
+
+app.get('*', function(req, res, next){
+  res.render('blah.ejs', {});
+})
 
 
 
