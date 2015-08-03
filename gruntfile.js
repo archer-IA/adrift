89d4a6388ce93b41f2d23937ac3dd0a808e2a5d1
Alex <alexadeschamps@gmail.com>
2015-08-03 11:56:43 -0400

    gruntfile made

---
+++ gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt){
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: "app/assets/src/js/*.js",
+        dest: "app/assets/js/script.min.js"
+      },
+      dev: {
+        options: {
+          beatify: true,
+          mangle: false,
+          compress: false,
+          preserveComments: 'all'
+        },
+        src: "app/assets/src/js/*.js",
+        dest: "app/assets/js/script.min.js"
+      }
+    },
+    watch: {
+      js: {
+        files: ['app/assets/src/js/*.js'],
+        tasks: ['uglify:dev']
+      },
+      css: {
+        files: ['app/assets/src/scss/**/*.scss'],
+        tasks: ['sass:dev']
+      }
+    },
+    sass: {
+      dev: {
+        options: {
+          outputStyle: 'expanded'
+        },
+        files: {
+          'app/assets/css/styles.css' : 'app/assets/src/scss/**/*.scss'
+        } 
+      },
+      build: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          'app/assets/css/styles.css' : 'app/assets/src/scss/**/*.scss'
+        }
+      }
+    }
+  })
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+
+  grunt.registerTask('default', ['uglify:dev', 'sass:dev']);
+  grunt.registerTask('build', ['uglify:build']);
+  grunt.registerTask('dev', ['uglify:dev', 'sass:build']);
+
+}
