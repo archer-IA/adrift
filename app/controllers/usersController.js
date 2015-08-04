a63b812088ab789b4527c5c103f4dd4f4e056b94
Remina Greenfield <reminagreenfield@gmail.com>
2015-08-04 11:50:35 -0400

    adds user controller

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -1,1 +1,58 @@
+var express = require('express'),
+     router = express.Router(),
+     User   = require('../models/user.js');
+
+
+// index
+router.get('/', function(req,res){
+  User.find({}, function(err, users){
+    res.json(users);
+  });
+});
+
+// show 
+router.get('/:id', function(req, res){
+  User.findById(req.params.id, function(err, user){
+    if (err){
+      res.json({status: 'failure'});
+    } else {
+      res.json({user: user, status: 'success');
+    }
+  });
+});
+
+// create
+router.post('/', function(req,res){
+  User.create(req.body.user, function(err, user){
+    if (err){
+      res.json({status: 'failure'});
+    } else {
+      res.json({user: user, status: 'success');
+    }
+  });
+});
+
+// update
+router.patch('/:id', function(req, res){
+  User.findByIdAndUpdate(req.params.id, req.body.user, function(err, user){
+    if(err){
+      res.json({status: 'failure'});
+    } else {
+      res.json({user: user, status: 'success');
+    }
+  })
+})
+
+// destroy
+router.delete('/:id', function(req, res){
+  User.findByIdAndUpdate(req.params.id, {active: false}, function(err){
+    if(err){
+      res.json({status: 'failure'});
+    } else {
+      req.session.currentUser = null;
+      res.json({status: 'success'});
+    }
+  })
+})
+
 module.exports = {};
