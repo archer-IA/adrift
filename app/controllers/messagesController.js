58b84d8e08cf6f8b77b14617dcff9b8ab36f05dc
Alex <alexadeschamps@gmail.com>
2015-08-06 16:48:12 -0400

    quick fix on message controller

--- app/controllers/messagesController.js
+++ app/controllers/messagesController.js
@@ -52,7 +52,6 @@ router.post('/decision', function(req, res, next){
 router.post('/', function(req, res, next){
   var message = req.body.message;
   message._user = req.session.currentUser._id;
-  message.original_content = message.content || null;
   Topic.findByIdAndUpdate(message._topic, { $push: { messages: message} }, function(err, topic){
     if(err){
       res.json({status: 'failure', error: '500'});
