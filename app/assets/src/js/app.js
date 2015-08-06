21333ecaf62eb5fd21e0f8198c6b902d7bda21e3
Remina Greenfield <reminagreenfield@gmail.com>
2015-08-06 10:01:37 -0400

    fixes routes

--- app/assets/src/js/app.js
+++ app/assets/src/js/app.js
@@ -1,6 +1,5 @@
 var adriftApp = angular.module('adriftApp', [
   'ngRoute',
-  'sessionController',
   'userControllers',
   'topicControllers'
 ]);
@@ -9,9 +8,6 @@ adriftApp.config(['$routeProvider', '$locationProvider',
   function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode({ enabled: true });
     $routeProvider.
-      when('/user/logout', {
-        controller: 'SessionLogoutCtrl'
-      }).
       when('/locations', {
         templateUrl: 'partials/topics/index.html',
         controller: 'TopicIndexCtrl'
