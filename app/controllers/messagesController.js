1be7f16fce2977c6283f5a1576cad2dc9fc2f2d9
Alex <alexadeschamps@gmail.com>
2015-08-10 11:06:17 -0400

    fixed create message route

--- app/controllers/messagesController.js
+++ app/controllers/messagesController.js
@@ -63,7 +63,7 @@ router.post('/decision', function(req, res, next){
 router.post('/', function(req, res, next){
   var message = req.body.message;
   message._user = req.session.currentUser._id;
-  Topic.findByIdAndUpdate(message._topic, { $push: { messages: message} }, function(err, topic){
+  Topic.findAndUpdate({name: message.topicName}, { $push: { messages: message} }, function(err, topic){
     if(err){
       res.json({status: 'failure', error: '500'});
     }else if(topic){
