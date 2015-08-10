f8b03c3857b48339c160d7dc6ef5ecedbd070fcd
Alex <alexadeschamps@gmail.com>
2015-08-10 11:10:45 -0400

    creating messages definetley working now

--- app/controllers/messagesController.js
+++ app/controllers/messagesController.js
@@ -63,7 +63,7 @@ router.post('/decision', function(req, res, next){
 router.post('/', function(req, res, next){
   var message = req.body.message;
   message._user = req.session.currentUser._id;
-  Topic.findAndUpdate({name: message.topicName}, { $push: { messages: message} }, function(err, topic){
+  Topic.findOneAndUpdate({name: message.topicName}, { $push: { messages: message} }, function(err, topic){
     if(err){
       res.json({status: 'failure', error: '500'});
     }else if(topic){
